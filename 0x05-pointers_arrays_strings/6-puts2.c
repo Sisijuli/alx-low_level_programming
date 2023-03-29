9ee717f2a0ff0c32d78ea21a92cffcd5db7ac990
sisijuli <ajaojuliana31@gmail.com>
2023-03-28 18:39:18 -0700

    101-keygen

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
-* puts2 - function should print only one character out of two
-* starting with the first one
-* @str: input
-* Return: print
-*/
+ * puts2 - function should print only one character out of two
+ * starting with the first one
+ * @str: input
+ * Return: print
+ */
 void puts2(char *str)
 {
-int longi = 0;
+int longi 0;
 int t = 0;
 char *y = str;
 int o;
 while (*y != '\0')
 {
-Y++;
+y++;
 longi++;
 }
 t = longi - 1;
@@ -25,3 +25,4 @@ _putchar(str[o]);
 }
 }
 _putchar('\n');
+}
